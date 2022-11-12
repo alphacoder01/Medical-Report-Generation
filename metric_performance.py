72033c49ab9a152c48fd2b1ac1fa31f29a8feeeb
swainsubrat <mailofswainsubrat@gmail.com>
2022-11-12 22:30:52 +0530

    updated eval code

--- metric_performance.py
+++ metric_performance.py
@@ -25,25 +25,32 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--result_path', type=str,
-                        default='/home/sweta/adv_cv_project/Medical-Report-Generation/try3/v4/20221107-11:09/results/cleaned_train_res.json')
+                        default='../ckpt1/result.json')
+
+    parser.add_argument('--gt_path', type=str,
+                        default='./test_set_cap.json')
+    
     args = parser.parse_args()
 
     test = load_json(args.result_path)
+    gt = load_json(args.gt_path)
+
     datasetGTS = {'annotations': []}
     datasetRES = {'annotations': []}
 
-    for i, image_id in enumerate(test):
+    i = 0
+    for image_id, image_id_gt in zip(test,gt):
         array = []
-        for each in test[image_id]['Pred Sent']:
-            array.append(test[image_id]['Pred Sent'][each])
+        for each in test[image_id]:
+            array.append(test[image_id][each])
         pred_sent = '. '.join(array)
-
+    
         array = []
-        for each in test[image_id]['Real Sent']:
-            sent = test[image_id]['Real Sent'][each]
-            if len(sent) != 0:
-                array.append(sent)
+        array.append(gt[image_id_gt] if not type(gt[image_id_gt])==float else '.')
         real_sent = '. '.join(array)
+
+
+
         datasetGTS['annotations'].append({
             'image_id': i,
             'caption': real_sent
@@ -52,6 +59,7 @@ if __name__ == '__main__':
             'image_id': i,
             'caption': pred_sent
         })
+        i+=1
 
     rng = range(len(test))
     print(calculate_metrics(rng, datasetGTS, datasetRES))
